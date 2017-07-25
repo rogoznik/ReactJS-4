068dc586d65c6caa1b66eb782700f3e277d4702e
rogoznik <nikrogoz@yandex.ru>
2017-07-25 14:51:34 +0500

    hw1

---
+++ hw1/script.js
@@ -0,0 +1,141 @@
+/********************************************************************
+ *
+ *  Задание 1
+ *
+ ********************************************************************/
+
+function loop(times = 0, callback = null) {
+  if (callback !== null){
+    for (let i = 0; i < times; i++) {
+      callback(2, 3);
+    }
+  }
+}
+console.log('Задание 1');
+loop(3, (a, b) => console.log(a + b));
+
+/********************************************************************
+ *
+ *  Задание 2
+ *
+ ********************************************************************/
+
+function calculateArea(figure, a = 0, b = 0, c = 0){
+  let area = 0;
+  let result = {};
+  if (figure === 'rectangle') {
+    area = a * b;
+    result = {
+      area: area,
+      figure: 'Прямоугольник',
+      input: {a: a, b: b}
+    };
+  }
+  if (figure === 'circle') {
+    area = Math.PI * (a*a);
+    result = {
+      area: area,
+      figure: 'Круг',
+      input: {a: a}
+    };
+  }
+  if (figure === 'triangle') {
+    let p = (a + b + c) / 2;
+    area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    result = {
+      area: area,
+      figure: 'Треугольник',
+      input: {a: a, b: b, c: c}
+    };
+  }
+
+  return result;
+}
+
+/********************************************************************
+ *
+ *  Задание 3
+ *
+ ********************************************************************/
+
+class Human {
+  constructor(name, age, dateOfBirth){
+    this.name = name;
+    this.age = age;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  displayInfo(){
+    return this.name + ' ' + this.age + ' ' + this.dateOfBirth;
+  }
+}
+class Employee extends Human {
+  constructor( name, age, dateOfBirth, salary, department){
+    super(name,  age, dateOfBirth);
+    this.salary = salary;
+    this.department = department;
+  }
+
+  displayInfo(){
+    return super.displayInfo() + ' ' + this.salary + ' ' + this.department;
+  }
+}
+
+class Developer extends Employee {
+  constructor(name, age, dateOfBirth, salary, department){
+    super(name,  age, dateOfBirth, salary, department);
+    this.manager = null;
+  }
+
+  setManager(manager) {
+    if (this.manager !== manager){
+      manager.addDeveloper(this);
+      this.manager = manager;
+    }
+  }
+
+  removeManager() {
+    this.manager.developers.splice(this.manager.developers.indexOf(this), 1);
+    this.manager = null;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, age, dateOfBirth, salary, department){
+    super(name,  age, dateOfBirth, salary, department);
+    this.developers = [];
+  }
+
+  addDeveloper(developer){
+    let index = this.developers.indexOf(developer);
+    if (index == -1){
+      this.developers.push(developer);
+      developer.setManager(this);
+    }
+  }
+
+  removeDeveloper(developer){
+    let index = this.developers.indexOf(developer);
+    if (index != -1) {
+      developer.removeManager();
+    }
+  }
+}
+
+let dev = new Developer('developer', 12, '12.12.12', 5, 'asdasasd');
+let mngr = new Manager('manager', 20, '01.01.01', 10, 'qweqweqwe');
+
+// dev.setManager(mngr);
+mngr.addDeveloper(dev);
+console.log('Задание 3');
+console.log(dev.displayInfo());
+console.log(mngr.displayInfo());
+console.log(dev);
+console.log(mngr);
+
+dev.removeManager();
+// mngr.removeDeveloper(dev);
+console.log(dev);
+console.log(mngr);
+
+
